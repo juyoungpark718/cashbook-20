84e52576ea9606d09a72f2387284cdfadb00f199
Juyoung Park <juyoung7018.park@gmail.com>
2021-07-28 22:52:16 +0900

    ⭐️ feat: categoryRouter 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const userRouter = require('./user');
 const categoryRouter = require('./category');
 const historyRouter = require('./history');
+const categoryRouter = require('./category');
 
 /* GET home page. */
 router.use(userRouter);
 router.use(categoryRouter);
 router.use(historyRouter);
+router.use(categoryRouter);
 
 module.exports = router;
