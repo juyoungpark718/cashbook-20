77c39e6b6e62da61a0af16b6879666e7a93baaed
김지용 <jyong9591@gmail.com>
2021-07-29 15:53:19 +0900

    ✔️ chore: invalid token 메시지 추가

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,11 +1,13 @@
 interface IConstant {
   LOGIN_REQUIRED: string;
   LOGIN_FAILED: string;
+  INVALID_TOKEN: string;
 }
 
 const constant: IConstant = {
   LOGIN_REQUIRED: '🔒 로그인이 필요합니다. 로그인 페이지로 이동합니다.',
   LOGIN_FAILED: '⚠️ 로그인에 실패하셨습니다.',
+  INVALID_TOKEN: '⌛️ 토큰이 만료되었습니다. 다시 로그인 해주세요.',
 };
 
 export default constant;
